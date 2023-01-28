1429611a1f5f45d1fd7b3ce3e6efb2084024b75d
ByteThisTech <hello@nickcarvajal.com>
2023-01-27 21:33:31 -0500

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 
 import { Canvas } from '@react-three/fiber';
-import { Html } from '@react-three/drei';
+import { Html,PerspectiveCamera } from '@react-three/drei';
 import { Suspense } from 'react';
 import './App.css'
 import IndexScene  from './IndexScene';
+import {getProject} from '@theatre/core';
+import studio from '@theatre/studio';
+import extension from '@theatre/r3f/dist/extension';
+import {SheetProvider} from '@theatre/r3f';
+
+studio.initialize()
+studio.extend(extension)
+const demoSheet = getProject('Demo Project').sheet('Demo Sheet')
+
+
 export default function App() {
+
  
+
   return (
     
-     <Canvas shadows dpr={[1, 2]} camera={{ position: [1, 4, 5], fov: 60 }}>
+     <Canvas shadows dpr={[1, 2]} gl={{preserveDrawingBuffer:true}}>
+        <SheetProvider sheet={getProject('Demo Project').sheet('Demo Sheet')}>
+        <PerspectiveCamera theatreKey="Camera" makeDefault position={[1, 4, 5]} fov={75}/>
+
+
         <Suspense fallback={null}>
             <IndexScene />
         </Suspense>
-        
+        </SheetProvider>    
       </Canvas>
-   
+    
 
   )
 }
--- src/IndexScene.tsx
+++ src/IndexScene.tsx
@@ -1,12 +1,6 @@
 import { Stage, ContactShadows, AccumulativeShadows, Backdrop, RandomizedLight, Sphere, Sky, useTexture, useGLTF, OrbitControls, Environment, Plane, PerspectiveCamera, GradientTexture, MeshReflectorMaterial } from "@react-three/drei";
-
-import { Ape_Animation } from "./assets/BoredApe_Animations";
-
-import { Ape_NoAnimation } from "./assets/BoredApe_NoAnimations";
-
-import { Manequin_Animated_01t } from "./assets/Manequin_Animated_01t";
 import * as THREE from "three";
-
+import { Model } from "./assets/YootAndGoblin2";
 import { useRef } from "react";
 
 import { EffectComposer, Bloom, Noise, DepthOfField } from '@react-three/postprocessing'
@@ -29,29 +23,29 @@ export default function IndexScene(){
 
       {/* Environment ====================================== */}
 
-      <mesh>
+      {/* <mesh>
         <Environment preset="warehouse" background />   
-      </mesh>
+      </mesh> */}
 
       {/* Lights =========================================== */}
      
        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={.5} shadow-mapSize={[512, 512]} castShadow />
        <ContactShadows position={[0, -1.4, 0]} opacity={0.75} scale={10} blur={2.5} far={4} />
-       <pointLight position={[-10, 0, -20]} color="white" intensity={1} />
-       <pointLight position={[0, -10, 0]} intensity={1} />
+       {/* <pointLight position={[-10, 0, -20]} color="white" intensity={.5} />
+       <pointLight position={[0, -10, 0]} intensity={1} /> */}
 
-       <ambientLight intensity={0.5} />
+       <ambientLight intensity={1} />
 
        {/* Backdrop Scene  ================================= */}
 
        <Backdrop receiveShadow castShadow floor={2} position={[0, 0, -2]} scale={[50, 10, 4]}>
-        <meshStandardMaterial color="#353540" envMapIntensity={0.1} />
+        <meshStandardMaterial color="#101010" envMapIntensity={0.1} />
        </Backdrop>
 
       {/* Postprocessing =================================== */}
        <EffectComposer disableNormalPass>
-        {/* <Bloom luminanceThreshold={0} mipmapBlur luminanceSmoothing={0.0} intensity={2} /> */}
-        {/* <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} /> */}
+        <Bloom luminanceThreshold={0} mipmapBlur luminanceSmoothing={0.0} intensity={.3} />
+        <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
         <Noise opacity={0.025} />
        </EffectComposer>
 
@@ -62,7 +56,7 @@ export default function IndexScene(){
       {/* Models =========================================== */}
        
        {/* <Ape_NoAnimation position={[0, .01, 0]} scale={2}/>  */}
-       <Manequin_Animated_01t position={[0, .01, 0]} scale={2}/> 
+       <Model position={[0, .01, 0]} scale={2}/> 
        
       
       
